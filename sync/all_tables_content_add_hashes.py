9158186f242c2165117405e6b704a5a9a5980018
Lucille Delisle <lucille.delisle@unige.ch>
2026-07-16 13:09:46 +0200

    fix the python script

--- sync/all_tables_content_add_hashes.py
+++ sync/all_tables_content_add_hashes.py
@@ -113,27 +113,28 @@ def iter_matching_files(pathspec):
         )
 
 
-def hash_one(args):
+def hash_one(f_no_hash):
     """
     get the info stored in the manifest (path, size, hash, symlink)
     """
+    args, no_hash = f_no_hash
     rel, path = args
     size = path.stat().st_size
-    if args.no_hash:
+    if no_hash:
         digest = ""
     else:
         digest = file_hash(path)
     return {"path": rel, "size": size, "digest": digest, "symlink": path.is_symlink()}
 
 
-def manifest_and_hash(root, max_workers=8):
+def manifest_and_hash(root, max_workers=8, no_hash=False):
     """
     comput manifest (all contents + metainfo) and a global hash
     """
     files = iter_matching_files(root)
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        manifest = list(executor.map(hash_one, [f for f in files]))
+        manifest = list(executor.map(hash_one, [(f, no_hash) for f in files]))
 
     # Compute the master hash in deterministic order
     master = hashlib.sha256()
@@ -168,7 +169,7 @@ for table_name in all_tables_content:
         if path is None:
             continue
         try:
-            entry["manifest"], entry["digest"] = manifest_and_hash(path, args.threads)
+            entry["manifest"], entry["digest"] = manifest_and_hash(path, args.threads, args.no_hash)
         except Exception as e:
             logger.error(f"Could not compute digest of {path}: {e}")
             continue
